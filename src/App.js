57b41ba13b8be44b99e465c8090c86be28c52b1c
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-12-23 21:07:08 +0600

    init

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ function App() {
     return (
         <div className="App">
             <MyTestComponent/>
-            <hr/>
             <Header/>
             <Routes>
                 <Route index element={<ProductList/>}/>
--- src/components/myTestComponent.jsx
+++ src/components/myTestComponent.jsx
@@ -5,10 +5,12 @@ const tg = window.Telegram.WebApp
 function MyTestComponent() {
     // const text1 = `Hello, ${user?.first_name} ${user?.last_name},\n ${user?.username}!`
     const text2 = tg.initDataUnsafe
+    const text3 = tg.initDataUnsafe?.user
     return (
         <>
             <div>
                 {text2}
+                {text3}
             </div>
         </>
     );
