d4202451372d0bb9a42867b282728edfeab8b3b7
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-12-23 21:29:55 +0600

    init

--- src/components/myTestComponent.jsx
+++ src/components/myTestComponent.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-// const tg = window.Telegram.WebApp
+const tg = window.Telegram.WebApp
 
 function MyTestComponent() {
     // const text1 = `Hello, ${user?.first_name} ${user?.last_name},\n ${user?.username}!`
     // const text2 = tg.initDataUnsafe
-    // const text3 = tg.initDataUnsafe?.user
+    const text3 = tg.initDataUnsafe?.user
     return (
         <>
-            Привет!
+            Привет! Вывожу через JSON.stringify(text3)
             {/*<div>*/}
             {/*    /!*{text2}*!/*/}
             {/*    Вывожу через JSON.stringify(text3)*/}
-            {/*    {JSON.stringify(text3)}*/}
+            {JSON.stringify(text3)}
             {/*</div>*/}
         </>
     );
