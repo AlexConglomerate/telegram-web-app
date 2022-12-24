182957bc805207229126402de3e6f18627b16455
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2022-12-24 11:22:44 +0600

    init

--- src/components/myTestComponent.jsx
+++ src/components/myTestComponent.jsx
@@ -4,15 +4,15 @@ const tg = window.Telegram.WebApp
 
 function MyTestComponent() {
     // const text1 = `Hello, ${user?.first_name} ${user?.last_name},\n ${user?.username}!`
-    const text2 = tg.initDataUnsafe
+    // const text2 = tg
     // const text3 = tg.initDataUnsafe?.user
     return (
         <>
-            Привет! Вывожу через JSON.stringify(text2)
+            Привет! Вывожу через JSON.stringify(tg)
             {/*<div>*/}
             {/*    /!*{text2}*!/*/}
             {/*    Вывожу через JSON.stringify(text3)*/}
-            {JSON.stringify(text2)}
+            {JSON.stringify(tg)}
             {/*</div>*/}
         </>
     );
